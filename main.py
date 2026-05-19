8599675bcf845058241b288724f7b7787dbf0e73
jyothsnap51-web <jyothsnap51@gmail.com>
2026-05-19 09:39:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+a=20
+b=33
+c=0
+if a and b and c:
+    print("all the numbers have boolean value as true")
+else:
+    print("atleast one number will have boolean value as true")
+
+
+
+a=21
+b=18
+c=0
+if a>b or b>c:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+
+if b>0 or c>0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
